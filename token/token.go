40483a74fe908bf22766799d40abe7ef972451ae
jjuranek <jarek.juranek99@gmail.com>
2024-09-10 20:37:33 +0200

    Add first part of accepted tokens

--- token/token.go
+++ token/token.go
@@ -6,3 +6,31 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+// Recognized possible tokens:
+const (
+	// SEPCIAL
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// TYPES
+	IDENT = "IDENT" // identifier, like x in let x =...
+	INT   = "INT"
+
+	// OPERATORS
+	ASSIGN = "="
+	ADD    = "+"
+
+	// DELIMITERS, CODE STRUCTURE
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// LANGUAGE KEYWOARDS
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
